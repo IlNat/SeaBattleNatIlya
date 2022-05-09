b44acc88c729843e3131e92ac73036085cf2267f
IlNat <ilanatalcenko18@gmail.com>
2022-05-09 19:43:22 +0500

    Final build of program.

--- SBG_script.cpp
+++ SBG_script.cpp
@@ -6,7 +6,7 @@ char user_field[10][10];
 char computer_field[10][10];
 char empty_computer_field[10][10];
 
-bool is_player_move;
+bool is_player_move = true;
 
 void fill_empty_computer_field()
 {
@@ -110,7 +110,7 @@ int check(char field[10][10]) {
 	int kolvo = 0;
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
-			if (field[i][j] == '*') {
+			if (field[i][j] == (char)42) {
 				kolvo++;
 			}
 		}
@@ -118,85 +118,129 @@ int check(char field[10][10]) {
 	return kolvo;
 }
 
+int check_for_mistakes(char field[10][10]) {
+	int mistake = 0;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (field[i][j] == (char)42) {//(char)42 = '*'
+				if (j < 6 && field[i][j] == field[i][j + 1] && field[i][j + 1] == field[i][j + 2] && field[i][j + 2] == field[i][j + 3] && field[i][j + 4])//(char)42 = '*'
+					mistake = 1;
+				if (i < 6 && field[i][j] == field[i + 1][j] && field[i + 1][j] == field[i + 2][j] && field[i + 2][j] == field[i + 3][j] && field[i + 4][j])
+					mistake = 2;
+				if (j < 9 && i < 9 && j > 0 && i > 0 && (field[i][j] == field[i + 1][j + 1] || field[i][j] == field[i - 1][j + 1] || field[i][j] == field[i - 1][j - 1] || field[i][j] == field[i + 1][j - 1]))
+					mistake = 3;
+				if (j == 9 && i < 9 && (field[i][j] == field[i + 1][j - 1] || field[i][j] == field[i - 1][j - 1]))
+					mistake = 4;
+				if (j < 9 && i == 9 && (field[i][j] == field[i + 1][j - 1] || field[i][j] == field[i - 1][j - 1]))
+					mistake = 5;
+				if (j < 9 && i < 9 && i > 0 && j > 0 && ((field[i][j] == field[i + 1][j] && field[i][j] == field[i][j + 1]) || (field[i][j] == field[i - 1][j] && field[i][j] == field[i][j + 1]) || (field[i][j] == field[i + 1][j] && field[i][j] == field[i][j - 1]) || (field[i][j] == field[i - 1][j] && field[i][j] == field[i][j - 1])))
+					mistake = 6;
+				if (j == 0 && i == 0 && field[i][j] == field[i + 1][j] && field[i][j] == field[i][j + 1])
+					mistake = 7;
+				if (j == 9 && i == 0 && field[i][j] == field[i + 1][j] && field[i][j] == field[i][j - 1])
+					mistake = 8;
+				if (j == 0 && i == 9 && field[i][j] == field[i - 1][j] && field[i][j] == field[i][j + 1])
+					mistake = 9;
+				if (j == 9 && i == 9 && field[i][j] == field[i - 1][j] && field[i][j] == field[i][j - 1])
+					mistake = 10;
+			}
+		}
+	}
+	return mistake;
+}
+
 bool error(char field[10][10]) 
 {	
 	if (check(field) == 20) {
 		return false;
 	}
+	else if (check_for_mistakes(field) != 0) {
+		cout << "Код ошибки: " << check_for_mistakes(field) << '\n';
+		return true;
+	}
 	else {
+		cout << "Код ошибки: 0\n";
 		return true;
 	}
 }
 
+int check_for_ships(char field[10][10]) {
+	int kolvo = 0;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (field[i][j] == (char)43) {
+				kolvo++;
+			}
+		}
+	}
+	return kolvo;
+}
+
 int game_over() {
-	if (check(user_field) == 0) 
+	if (check_for_ships(user_field) == 20) 
 		return 1;
-	else if (check(computer_field) == 0) 
+	else if (check_for_ships(empty_computer_field) == 20) 
 		return 2;
 	else
 		return 0;
 }
 
-void make_player_move() {
+int make_player_move() {
 	int fstcord, scdcord;
 	do {
 		cout << "Введите координаты для атаки (x [1; 10]; y [1; 10]): \n";
 		cin >> fstcord >> scdcord;
-		if (empty_computer_field[fstcord][scdcord] != '#')
+		fstcord--;
+		scdcord--;
+		if (empty_computer_field[fstcord][scdcord] != '#' || fstcord > 9 || fstcord < 0 || scdcord < 0 || scdcord > 9)
 			cout << "Введите другие координаты.\n";
-	} while (empty_computer_field[fstcord][scdcord] != '#');
-	fstcord--;
-	scdcord--;
+	} while (empty_computer_field[fstcord][scdcord] != '#' || fstcord > 9 || fstcord < 0 || scdcord < 0 || scdcord > 9);
 	if (computer_field[fstcord][scdcord] == '#') {
 		cout << "Промах.\n";
 		empty_computer_field[fstcord][scdcord] = (char)45;
+		return 0;
 	}
 	if (computer_field[fstcord][scdcord] == '*') {
-		if (computer_field[fstcord][scdcord] == computer_field[fstcord][scdcord + 1] || computer_field[fstcord][scdcord] == computer_field[fstcord][scdcord - 1] || computer_field[fstcord][scdcord] == computer_field[fstcord + 1][scdcord] || computer_field[fstcord][scdcord] == computer_field[fstcord - 1][scdcord]) {
-			cout << "Попадание.\n";
-			empty_computer_field[fstcord][scdcord] = (char)43;
-		}
-		else {
-			cout << "Корабль уничтожен.";
-		}
+		cout << "Попадание.\n";
+		empty_computer_field[fstcord][scdcord] = (char)43;
+		return 1;
 	}
 }
 
-void make_computer_move() {
+int make_computer_move() {
 	// Ход компьютера.
-	int fstcord;
-	int scdcord{};
+	int fstcord, scdcord;
 	do {
 		fstcord = rand() % 10;
 		scdcord = rand() % 10;
-	} while (user_field[fstcord][scdcord] != '#');
-	if (user_field[fstcord][scdcord] == '#')
-		user_field[fstcord][scdcord] = (char)45;
-	if (user_field[fstcord][scdcord] == '*')
-		user_field[fstcord][scdcord] = (char)43;
+	} while (user_field[fstcord][scdcord] != '#' && user_field[fstcord][scdcord] != '*');
+	if (user_field[fstcord][scdcord] == '#') {
+		user_field[fstcord][scdcord] = (char)45;// (char)45 = '-'
+		return 0; 
+	}
+	else {
+		user_field[fstcord][scdcord] = (char)43;// (char)43 = '+'
+		return 1;
+	}
 }
 
 void make_next_move() {
 	if (is_player_move) {
 		make_player_move();
-		is_player_move = false;
+		if (make_player_move() == 1)
+			is_player_move = true;
+		else
+			is_player_move = false;
 	}
-	else {
+	if(!is_player_move) {
 		make_computer_move();
-		is_player_move = true;
+		if (make_computer_move() == 0)
+			is_player_move = true;
+		else
+			is_player_move = false;
 	}
 	
 }
-//char move_player_move() {
-//	int row, col;
-//	cout << "Введите координаты цели x[]\n";
-//	return '\0';
-//}
-/*if (field[i][j] == '*') {
-					if (field[i][j] == field[i++][j++] || field[i][j] == field[i--][j--] || field[i][j] == field[i++][j--] || field[i][j] == field[i--][j++]) {
-						return true;
-					}
-				}*/
 
 void game() {
 	fill_all_fields();
@@ -230,6 +274,7 @@ void game() {
 void run()
 {
 	setlocale(LC_ALL, "russian");
+	srand(time(nullptr));
 	cout << "Добро пожаловать в игру \"Морской бой\".\n";
 	cout << "Внимание! Перед началом игры проверьте все игровые поля(файлы \"CBF.txt\" и \"UBF.txt\" в папке с программой). В случае несоответствия расстановки элементов в игровых полях компьютер выдаст ошибку с ссылкой на файл.\n";
 	cout << "Для начала игры введите 1, для выхода из программы введите 2.\n";
